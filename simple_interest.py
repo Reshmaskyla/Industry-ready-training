a805864f9a8f396f9a2e43aae5d39a4e58abe82d
Reshmaskyla <reshmaskyla@gmail.com>
2022-05-05 22:48:37 +0530

    simple_interest

---
+++ simple_interest.py
@@ -0,0 +1,6 @@
+principle = int(input("Enter principle :"))
+tenure = 5
+rate_of_interest = 0.9
+simple_interest = (principle * tenure * rate_of_interest) / 100
+
+print("The simple interest is:",simple_interest)
