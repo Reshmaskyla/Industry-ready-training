c76dabafc80180410cd29b7870a012c5344cf65f
Reshmaskyla <reshmaskyla@gmail.com>
2022-05-09 15:49:13 +0530

    volume_of_cylinder

---
+++ 8.py
@@ -0,0 +1,5 @@
+Pi = 3.14
+r = int(input("enter the cylinder radius : "))
+h = int(input("enter the height of cylinder : "))
+volume_of_cylinder =  Pi * r * r * h
+print("the volume of cylinnder : ", volume_of_cylinder)
